f54c95d0e1d746d0ad82cb3133af7d3d4987ad52
Cailie Skelton <cskelton707@gmail.com>
2020-04-22 20:49:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
-def reverse_each_word(sentence)
-    words = sentence.split(" ")
-    new_words = []
-    words.collect do |word| 
-    new_words << word.reverse} 
+def reverse_each_word(string)
+    convert_to_array = string.split(' ')
+    reversed_words=[]
+    convert_to_array.collect do |string|
+        reversed_words << string.reverse
     end
-    new_words.join(" ")
+    reversed_string=reversed_words.join(' ')
 end
+reverse_each_word("Where ever I go I dream about you")
